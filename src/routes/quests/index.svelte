590018c907a6f368b10679bce044325d714caef3
Nicholas Guyett <nguyett@hoplabs.com>
2021-11-06 21:29:54 -0700

    Add quest page mockups

---
+++ src/routes/quests/index.svelte
@@ -0,0 +1,25 @@
+<style>
+  .quest-list {
+    padding: 0;
+    list-style: none;
+  }
+</style>
+<script>
+  // Get some tasks
+</script>
+<h2>My Quests</h2>
+<nav>
+  <ul>
+    <li><button type="button" class="primary">Start New Quest!</button></li>
+  </ul>
+</nav>
+<ul class="quest-list">
+  <li>
+    <article class="quest-summary">
+      <header>Finish Quest MVP</header>
+      <p>This app you're looking at?  Make it work</p>
+      <button type="button" class="primary">Complete</button>
+      <button type="button" class="error">Abandon</button>
+    </article>
+  </li>
+</ul>
