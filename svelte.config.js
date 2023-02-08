6043f3aafe2a2eeea3e0144915fa496312eb381b
Nicholas Guyett <nicholas@guyett.me>
2023-02-08 15:48:39 -0800

    Upgrade to latest svelte-kit

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,15 @@
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: [
-    preprocess({
-      postcss: true
-    })
-  ],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess({postcss: true}),
 
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte'
-  }
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
