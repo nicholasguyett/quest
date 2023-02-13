f47097ed4092bcc6aa8089b02f77a8b43ac06c8d
Nicholas Guyett <nicholas@guyett.me>
2023-02-13 14:43:27 -0800

    Add ability to mark quests completed or abandon quests

--- src/lib/QuestSummary.svelte
+++ src/lib/QuestSummary.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
+	import { db } from "./db";
 	import type { Quest } from "./quest";
 
 	export let value: Quest;
   console.log(value)
+
+  function completeQuest() {
+    db.quests.put({...value, is_completed: true});
+  }
+
+  function abandonQuest() {
+    db.quests.delete(value.id);
+  }
 </script>
 
 <article class="quest-summary">
 	<header>{value.title}</header>
 	<p>{value.description}</p>
   {#if !value.is_completed}
-    <button type="button" class="primary">Complete</button>
-    <button type="button" class="danger">Abandon</button>
+    <button type="button" class="primary" on:click={completeQuest}>Complete</button>
+    <button type="button" class="danger" on:click={abandonQuest}>Abandon</button>
   {/if}
 </article>
