191ad1fe1fa9f6f55f47d08a7738bb8d384b21f1
Nicholas Guyett <nicholas@guyett.me>
2023-05-30 16:30:29 -0700

    Add workaround to prevent flaky ci tests

--- tests/offline-mode.spec.ts
+++ tests/offline-mode.spec.ts
@@ -1,7 +1,5 @@
 import { test, type BrowserContext } from '@playwright/test';
 
-const WEBKIT_PAUSE_MS = 5000;
-
 test.describe('offline mode', () => {
 	let context: BrowserContext;
 
@@ -12,8 +10,22 @@ test.describe('offline mode', () => {
 		// Allow application to install the service worker and populate the cache
 		const page = await context.newPage();
 		await page.goto('/');
-
 		await page.waitForLoadState('networkidle');
+		// Service worker is not available on first load so we have to load the
+		// 		page again to make sure it is activated
+
+		// TODO: find alternative to 'networkidle'
+		await page.goto('/');
+		await page.waitForLoadState('networkidle');
+
+		// Make sure that the service worker is activated before going offline
+		await page.evaluate(async () => {
+			const registration = await window.navigator.serviceWorker.getRegistration();
+			if (registration?.active?.state === 'activated') return;
+			await new Promise((res) =>
+				window.navigator.serviceWorker.addEventListener('controllerchange', res)
+			);
+		});
 
 		// Set application to offline mode
 		await context.setOffline(true);
